5ca536f1f6b4479f8b452091ffc732775835e2c8
AmirHassenBenHassine <amirbh07@gmail.com>
2026-01-20 07:28:14 +0800

    Fixed wifi pairing and added list of networks in menu

--- src/screen.sh
+++ src/screen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 source /home/orangepi/venv/bin/activate
-exec sudo python3 /home/orangepi/screen-manager/src/stdby2.py
+exec sudo python3 /home/orangepi/screen-manager/src/stdby3.py
